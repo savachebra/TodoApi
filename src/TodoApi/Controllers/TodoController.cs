a5c858bc79db71c99beb2451537c1fbd4920edad
savachebra <savachebra@gmail.com>
2016-02-24 14:39:19 +0200

    ..

--- src/TodoApi/Controllers/TodoController.cs
+++ src/TodoApi/Controllers/TodoController.cs
@@ -44,5 +44,21 @@ namespace TodoApi.Controllers
 
             return CreatedAtRoute("GetTodo", new { controller = "Todo", id = item.Key }, item);
         }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody]TodoItem item)
+        {
+            if (item == null || item.Key != id)
+                return HttpBadRequest();
+
+            var todo = TodoItems.Find(id);
+
+            if (item == null)
+                return HttpNotFound();
+
+            TodoItems.Update(item);
+
+            return new NoContentResult();
+        }
     }
 }
